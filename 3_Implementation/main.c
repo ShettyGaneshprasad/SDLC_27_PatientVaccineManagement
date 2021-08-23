6849481ace9cbb72d2a4b9f12a148d0ce1f75fff
Shetty Ganeshprasad <shettyganeshprasad1998@gmail.com>
2021-08-23 20:27:25 +0530

    name refrence error resolved

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,48 +1,45 @@
 #include "Vaccination.h"
 
-
-int main(){
+int main()
+{
 
     int inserted_flag = 0;
     FILE *f = NULL;
     FILE *r = NULL;
     FILE *rt = NULL;
     patient *start = NULL;
-    error_t patient_indexfile = patient_indexFile("INDEX.DAT","rb+",&f);
-    
-    error_t recordFile = patient_openFile("RECORD.DAT","rb+", &r);
+    error_t patient_indexfile = patient_indexFile("INDEX.DAT", "rb+", &f);
+
+    error_t recordFile = patient_openFile("RECORD.DAT", "rb+", &r);
     int data_loaded_flag = 0;
     printf("PRESS 1 if you want to load File data or press 0 to continue: \n");
     int ch;
-    scanf("%d",&ch);
-    if(ch==1){
+    scanf("%d", &ch);
+    if (ch == 1)
+    {
         printf("LOADING DATA...\n");
         start = patient_loadData(start, &r);
         inserted_flag = 1;
         printf("DATA LOADED...\n");
     }
 
-    
     //int rsize = fseek(r,0, SEEK_END);
     // int size = ftell(r);
     // printf("SIZE OF RECORD.DAT is %d\n",size); // data is getting stored.
-    
 
-    
     // vaccine_data col_data = {0};
-    patient res ={0};
+    patient res = {0};
     int found;
-    
 
     // Declaring Function Pointers
-    patient *(*fptr_type1)(patient *,int, char *, char *, char *, float, float,int,char *, insured, vaccine) = create_patientid;
-    error_t (*fptr_type2)(patient *,int, char *, char *, char *, float, float,int,char *, insured, vaccine) = insert_patientrecords;
+    patient *(*fptr_type1)(patient *, int, char *, char *, char *, float, float, int, char *, insured, vaccine) = create_patientid;
+    error_t (*fptr_type2)(patient *, int, char *, char *, char *, float, float, int, char *, insured, vaccine) = insert_patientrecord;
     error_t (*fptr_type3)(patient *) = display_patientid;
     error_t (*fptr_type4)(patient *, int, patient *, int *) = find_by_patientid;
     error_t (*fptr_type5)(patient *, int, int, int) = update_patientrecord;
     error_t (*fptr_type6)(patient *, FILE **, int *) = patient_saveFile;
     int choice;
-    while(1)
+    while (1)
     {
         printf("Press 1 create a Linked List\n");
         printf("Press 2 to Insert a box at the end\n");
@@ -55,17 +52,17 @@ int main(){
         printf("Press -1 to exit.\n");
         printf("Enter your choice\n");
         scanf("%d", &choice);
-        
-        if(choice == -1){
+
+        if (choice == -1)
+        {
             break;
         }
 
         if (choice >= 1 && choice <= 10)
-        {   
+        {
 
-            
             int id;
-            char firstname[101]; 
+            char firstname[101];
             char lastname[101];
             char aadhar_num[21];
             float height;
@@ -76,90 +73,89 @@ int main(){
             vaccine shot;
 
             printf("%d is the choice\n", choice);
-            if(choice == 1){
-                
+            if (choice == 1)
+            {
+
                 printf("Enter Id of patient\n");
-                scanf("%d",&id);
+                scanf("%d", &id);
                 //step 1 validation to check existing ids
                 rewind(f);
-                error_t code =  validate_patientid(&f,id);
-                if(code == ID_EXISTS){
+                error_t code = validate_patientid(&f, id);
+                if (code == ID_EXISTS)
+                {
                     printf("Id already exist, choose something else\n");
                     continue;
                 }
                 printf("Enter First Name\n");
                 scanf("%100s", firstname);
                 printf("Enter the last name\n");
-                scanf("%100s",lastname);
+                scanf("%100s", lastname);
                 printf("Enter height of the patient\n");
-                scanf("%f",&height);
+                scanf("%f", &height);
                 printf("Enter weight of patient\n");
-                scanf("%f",&weight);
+                scanf("%f", &weight);
                 printf("enter aadhar card number\n");
-                scanf("%20s",aadhar_num);
+                scanf("%20s", aadhar_num);
                 printf("Enter age of the patient\n");
-                scanf("%d",&age);
+                scanf("%d", &age);
                 printf("Is the patient insured?\n");
-                scanf("%d",&insurance);
+                scanf("%d", &insurance);
                 printf("Enter the name of the vaccine recieved\n");
-                scanf("%d",&shot);
+                scanf("%d", &shot);
                 printf("Enter the date of vaccination\n");
                 scanf("%12s", date);
-                start = (*fptr_type1)(start,id,firstname,lastname,aadhar_num,height,weight,age,date,insurance,shot);
-                
+                start = (*fptr_type1)(start, id, firstname, lastname, aadhar_num, height, weight, age, date, insurance, shot);
 
                 // step 2 add index to file if not
-                fseek(f,0,SEEK_END);
-                fwrite(&id,4,1,f);
+                fseek(f, 0, SEEK_END);
+                fwrite(&id, 4, 1, f);
                 inserted_flag = 1;
+            }
 
-                
-                
-                }
-            
-        
-            
-            if(choice == 2){
+            if (choice == 2)
+            {
                 printf("Enter Id of patient\n");
-                scanf("%d",&id);
+                scanf("%d", &id);
                 //step 1 validation to check existing ids
                 rewind(f);
-                error_t file_code =  validate_patientid(&f,id);
-                if(file_code == ID_EXISTS){
+                error_t file_code = validate_patientid(&f, id);
+                if (file_code == ID_EXISTS)
+                {
                     printf("Id already exists, choose something else\n");
                     continue;
                 }
                 printf("Enter First Name\n");
                 scanf("%100s", firstname);
                 printf("Enter the last name\n");
-                scanf("%100s",lastname);
+                scanf("%100s", lastname);
                 printf("Enter height of the patient\n");
-                scanf("%f",&height);
+                scanf("%f", &height);
                 printf("Enter weight of patient\n");
-                scanf("%f",&weight);
+                scanf("%f", &weight);
                 printf("enter aadhar card number\n");
-                scanf("%20s",aadhar_num);
+                scanf("%20s", aadhar_num);
                 printf("Enter age of the patient\n");
-                scanf("%d",&age);
+                scanf("%d", &age);
                 printf("Is the patient insured?\n");
-                scanf("%d",&insurance);
+                scanf("%d", &insurance);
                 printf("Enter the name of the vaccine recieved\n");
-                scanf("%d",&shot);
+                scanf("%d", &shot);
                 printf("Enter the date of vaccination\n");
                 scanf("%12s", date);
-                error_t code = (*fptr_type2)(start,id,firstname,lastname,aadhar_num,height,weight,age,date,insurance,shot);
+                error_t code = (*fptr_type2)(start, id, firstname, lastname, aadhar_num, height, weight, age, date, insurance, shot);
                 // step 2 add index to file if not
-                fseek(f,0,SEEK_END);
-                fwrite(&id,4,1,f);
+                fseek(f, 0, SEEK_END);
+                fwrite(&id, 4, 1, f);
                 inserted_flag = 1;
-              
             }
 
-            if(choice == 3){
+            if (choice == 3)
+            {
                 printf("Please enter the id of the patient\n");
                 scanf("%d", &id); // perform validation
                 error_t code = (*fptr_type4)(start, id, &res, &found);
-                if(found == 1){
+                if (found == 1)
+                {
                     printf("Patient Record Found\n");
                     printf("first Name: %s\n", res.firstname);
                     printf("last Name: %s\n", res.lastname);
@@ -170,23 +166,26 @@ int main(){
                     printf("Insurance Status: %s\n", (res.insurance == 1) ? "Insured" : "Not Insured");
                     printf("Vaccine code: %d\n", res.shot);
                     printf("Date of vaccination is: %s\n", res.date_of_vaccine);
-
-                }else{
+                }
+                else
+                {
                     printf("Patient Record Not Found\n");
                 }
-                
             }
 
-            if(choice == 4){
+            if (choice == 4)
+            {
                 int choice_3;
                 printf("Press 1 to update File Record\n");
                 printf("Press 2 to update Disk Record\n");
-                scanf("%d",&choice_3);
-                if(2 == choice){
+                scanf("%d", &choice_3);
+                if (2 == choice)
+                {
                     printf("Please Enter the patient id:\n");
                     scanf("%d", &id);
-                    error_t code = (*fptr_type4)(start, id, &res ,&found);
-                    if(found == 1){
+                    error_t code = (*fptr_type4)(start, id, &res, &found);
+                    if (found == 1)
+                    {
                         printf("Patient Found with id: %d\n", res.uniq_id);
                         int choice_2;
                         printf("Please select the field you want to update\n");
@@ -200,42 +199,50 @@ int main(){
                         scanf("%d", &choice_2);
                         error_t code = (*fptr_type5)(start, id, choice_2, 0); // 0 is for prod run.
                         printf("**********Record Updated**********\n");
-                    }else{
+                    }
+                    else
+                    {
                         printf("NO RECORD FOUND\n");
                     }
-                }else{
+                }
+                else
+                {
                     printf("Please Enter the patient id:\n");
                     scanf("%d", &id);
                     rewind(f);
-                    error_t code = validate_patientid(&f,id);
-                    if(code == ID_EXISTS){
-                        update_patient_data_file(&r,id);
+                    error_t code = validate_patientid(&f, id);
+                    if (code == ID_EXISTS)
+                    {
+                        update_patient_data_file(&r, id);
                         printf("RECORD UPDATED\n");
-                    }else{
+                    }
+                    else
+                    {
                         printf("NO RECORD FOUND");
                     }
-
                 }
-                
             }
 
-            if(choice == 5){
+            if (choice == 5)
+            {
                 printf("Savind Data to file...\n");
                 error_t code = (*fptr_type6)(start, &r, &ch);
                 printf("Data Saved!\n");
-
             }
-            if(choice == 6){
+            if (choice == 6)
+            {
                 error_t code = patient_readFile(&r);
             }
 
-            if(choice == 7){
+            if (choice == 7)
+            {
                 printf("Please enter the id of the patient\n");
                 scanf("%d", &id); // perform validation
                 error_t code = (*fptr_type4)(start, id, &res, &found);
-                if(found == 1){
-                    start = delete_patientid(start,id);
-                    error_t del_code_2 = delete_patientrecords_file(&r,&rt,id);
+                if (found == 1)
+                {
+                    start = delete_patientid(start, id);
+                    error_t del_code_2 = delete_patientrecords_file(&r, &rt, id);
 
                     // Performing clean up
                     fclose(rt);
@@ -243,64 +250,55 @@ int main(){
                     remove("RECORD.DAT");
                     rename("TEMP.DAT", "RECORD.DAT");
 
-                    patient_openFile("RECORD.DAT","rb+",&r);
+                    patient_openFile("RECORD.DAT", "rb+", &r);
 
                     //Updating INDEX file
-                    error_t del_idx_code = delete_index_file(&f,&rt,id);
+                    error_t del_idx_code = delete_index_file(&f, &rt, id);
                     //fclose(rt);
                     fclose(f);
                     remove("INDEX.DAT");
                     rename("TEMP.DAT", "INDEX.DAT");
 
-                    patient_openFile("INDEX.DAT","rb+",&f);
+                    patient_openFile("INDEX.DAT", "rb+", &f);
+                }
+                else
+                {
+                    error_t del_code = delete_patientrecords_file(&r, &rt, id);
 
-                }else{
-                    error_t del_code = delete_patientrecords_file(&r,&rt,id);
-                    
                     // Updating RECORD file
                     //fclose(rt);
                     fclose(r);
                     remove("RECORD.DAT");
                     rename("TEMP.DAT", "RECORD.DAT");
 
-                    patient_openFile("RECORD.DAT","rb+",&r);
+                    patient_openFile("RECORD.DAT", "rb+", &r);
 
                     //Updating INDEX file
-                    error_t del_idx_code = delete_index_file(&f,&rt,id);
+                    error_t del_idx_code = delete_index_file(&f, &rt, id);
                     fclose(rt);
                     fclose(f);
                     remove("INDEX.DAT");
                     rename("TEMP.DAT", "INDEX.DAT");
 
-                    patient_openFile("INDEX.DAT","rb+",&f);
+                    patient_openFile("INDEX.DAT", "rb+", &f);
                 }
                 printf("Record Updated\n");
             }
 
-            if(choice == 10){
+            if (choice == 10)
+            {
                 printf("%d", start->uniq_id);
                 (*fptr_type3)(start);
-        
             }
-            
         }
-
-        
-  
-  
-    
     }
-    if(inserted_flag==1) (*fptr_type6)(start, &r, &ch);
+    if (inserted_flag == 1)
+        (*fptr_type6)(start, &r, &ch);
 
     fclose(f);
     fclose(r);
     printf("Here deleting!\n");
     delete_allpatientrecords(start);
-    
-    
-
 
     return 0;
-
-    
 }
--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -11,7 +11,7 @@ void tearDown()
 }
 
 int id;
-char firstname[101]; 
+char firstname[101];
 char lastname[101];
 char aadhar_num[21];
 float height;
@@ -29,192 +29,185 @@ FILE *r = NULL;
 FILE *rt = NULL;
 patient *start = NULL;
 
-int old_count,count;
+int old_count, count;
 
-void check_openFile(void){
-    TEST_ASSERT_EQUAL(SUCCESS,patient_indexFile("INDEX.DAT","rb+",&f));
-    TEST_ASSERT_EQUAL(SUCCESS,patient_openFile("RECORD.DAT","rb+",&r));
-
+void check_openFile(void)
+{
+    TEST_ASSERT_EQUAL(SUCCESS, patient_indexFile("INDEX.DAT", "rb+", &f));
+    TEST_ASSERT_EQUAL(SUCCESS, patient_openFile("RECORD.DAT", "rb+", &r));
 }
 
-void test_create(void){
+void test_create(void)
+{
     id = 1;
     strcpy(firstname, "testuser1");
     strcpy(lastname, "lastuser1");
-    strcpy(aadhar_num,"123456789");
+    strcpy(aadhar_num, "123456789");
     height = 167.0;
     weight = 76.1;
     age = 34;
-    strcpy(date,"12-01-21");
+    strcpy(date, "12-01-21");
     insurance = 1;
     shot = 2;
-    
 
-    start = create_patientid(start,id,firstname,lastname,aadhar_num,height,weight,age,date,insurance,shot);
-    
-    fseek(f,0,SEEK_END);
-    fwrite(&id,4,1,f);
+    start = create_patientid(start, id, firstname, lastname, aadhar_num, height, weight, age, date, insurance, shot);
 
-    TEST_ASSERT_EQUAL(1,start->uniq_id);
-    TEST_ASSERT_EQUAL_STRING("testuser1",start->firstname);
-    TEST_ASSERT_EQUAL_STRING("lastuser1",start->lastname);
-    TEST_ASSERT_EQUAL_STRING("123456789",start->aadhar_num);
+    fseek(f, 0, SEEK_END);
+    fwrite(&id, 4, 1, f);
 
-
+    TEST_ASSERT_EQUAL(1, start->uniq_id);
+    TEST_ASSERT_EQUAL_STRING("testuser1", start->firstname);
+    TEST_ASSERT_EQUAL_STRING("lastuser1", start->lastname);
+    TEST_ASSERT_EQUAL_STRING("123456789", start->aadhar_num);
 }
 
-void test_file_initilizaion(void){
-
+void test_file_initilizaion(void)
+{
 }
 
-void test_insert(void){
+void test_insert(void)
+{
     // With head
     id = 2;
     strcpy(firstname, "testuser2");
     strcpy(lastname, "lastuser2");
-    strcpy(aadhar_num,"123456789");
+    strcpy(aadhar_num, "123456789");
     height = 167.0;
     weight = 76.1;
     age = 34;
-    strcpy(date,"12-01-21");
+    strcpy(date, "12-01-21");
     insurance = 0;
     shot = 1;
 
-    TEST_ASSERT_EQUAL(SUCCESS, insert_patientrecords(start,id,firstname,lastname,aadhar_num,height,weight,age,date,insurance,shot));
-    fseek(f,0,SEEK_END);
-    fwrite(&id,4,1,f);
+    TEST_ASSERT_EQUAL(SUCCESS, insert_patientrecord(start, id, firstname, lastname, aadhar_num, height, weight, age, date, insurance, shot));
+    fseek(f, 0, SEEK_END);
+    fwrite(&id, 4, 1, f);
 
     id = 3;
     strcpy(firstname, "testuser3");
     strcpy(lastname, "lastuser2");
-    strcpy(aadhar_num,"123456789");
+    strcpy(aadhar_num, "123456789");
     height = 167.0;
     weight = 76.1;
     age = 34;
-    strcpy(date,"12-01-21");
+    strcpy(date, "12-01-21");
     insurance = 0;
     shot = 1;
 
-    TEST_ASSERT_EQUAL(SUCCESS, insert_patientrecords(start,id,firstname,lastname,aadhar_num,height,weight,age,date,insurance,shot));
-    fseek(f,0,SEEK_END);
-    fwrite(&id,4,1,f);
-
-
+    TEST_ASSERT_EQUAL(SUCCESS, insert_patientrecord(start, id, firstname, lastname, aadhar_num, height, weight, age, date, insurance, shot));
+    fseek(f, 0, SEEK_END);
+    fwrite(&id, 4, 1, f);
 }
 
-void check_idx_file_insertion(void){
+void check_idx_file_insertion(void)
+{
     rewind(f);
-    TEST_ASSERT_EQUAL(ID_EXISTS,validate_patientid(&f,1));
+    TEST_ASSERT_EQUAL(ID_EXISTS, validate_patientid(&f, 1));
     rewind(f);
-    TEST_ASSERT_EQUAL(ID_EXISTS,validate_patientid(&f,2));
+    TEST_ASSERT_EQUAL(ID_EXISTS, validate_patientid(&f, 2));
     rewind(f);
-    TEST_ASSERT_EQUAL(ID_EXISTS,validate_patientid(&f,3));
+    TEST_ASSERT_EQUAL(ID_EXISTS, validate_patientid(&f, 3));
     rewind(f);
-    TEST_ASSERT_EQUAL(SUCCESS,validate_patientid(&f,4));
-
+    TEST_ASSERT_EQUAL(SUCCESS, validate_patientid(&f, 4));
 }
 
-void check_file_save(void){
+void check_file_save(void)
+{
     int ch = 0;
     TEST_ASSERT_EQUAL(SUCCESS, patient_saveFile(start, &r, &ch));
 }
 
-void check_file_display(void){
-        
-        TEST_ASSERT_EQUAL(SUCCESS, patient_readFile(&r));
+void check_file_display(void)
+{
+
+    TEST_ASSERT_EQUAL(SUCCESS, patient_readFile(&r));
 }
 
-void check_find_by_id(void){
-        patient res ={0};
-        id = 3;
-        int found = 0;
-        TEST_ASSERT_EQUAL(SUCCESS, find_by_patientid(start,id,&res,&found));
-        TEST_ASSERT_EQUAL(1,found);
-        id = 5;
-        find_by_patientid(start,id,&res,&found);
-        TEST_ASSERT_EQUAL(0,found);
+void check_find_by_id(void)
+{
+    patient res = {0};
+    id = 3;
+    int found = 0;
+    TEST_ASSERT_EQUAL(SUCCESS, find_by_patientid(start, id, &res, &found));
+    TEST_ASSERT_EQUAL(1, found);
+    id = 5;
+    find_by_patientid(start, id, &res, &found);
+    TEST_ASSERT_EQUAL(0, found);
 }
 
-void check_update_record(void){
-    TEST_ASSERT_EQUAL(SUCCESS,update_patientrecord(start,2,1,1));
-    TEST_ASSERT_EQUAL(SUCCESS,update_patientrecord(start,2,2,1));
-    TEST_ASSERT_EQUAL(SUCCESS,update_patientrecord(start,2,3,1));
-    TEST_ASSERT_EQUAL(SUCCESS,update_patientrecord(start,2,4,1));
-    TEST_ASSERT_EQUAL(SUCCESS,update_patientrecord(start,2,5,1));
-    TEST_ASSERT_EQUAL(SUCCESS,update_patientrecord(start,2,6,1));
-    TEST_ASSERT_EQUAL(SUCCESS,update_patientrecord(start,2,7,1));
-    
+void check_update_record(void)
+{
+    TEST_ASSERT_EQUAL(SUCCESS, update_patientrecord(start, 2, 1, 1));
+    TEST_ASSERT_EQUAL(SUCCESS, update_patientrecord(start, 2, 2, 1));
+    TEST_ASSERT_EQUAL(SUCCESS, update_patientrecord(start, 2, 3, 1));
+    TEST_ASSERT_EQUAL(SUCCESS, update_patientrecord(start, 2, 4, 1));
+    TEST_ASSERT_EQUAL(SUCCESS, update_patientrecord(start, 2, 5, 1));
+    TEST_ASSERT_EQUAL(SUCCESS, update_patientrecord(start, 2, 6, 1));
+    TEST_ASSERT_EQUAL(SUCCESS, update_patientrecord(start, 2, 7, 1));
 }
 
-void check_delete_node(void){
+void check_delete_node(void)
+{
     old_count = 1;
     patient *ptr;
     ptr = start;
-    while(ptr->next != NULL){
+    while (ptr->next != NULL)
+    {
         old_count++;
         ptr = ptr->next;
     }
-    TEST_ASSERT_EQUAL(3,old_count);
-    start = delete_patientid(start,3);
+    TEST_ASSERT_EQUAL(3, old_count);
+    start = delete_patientid(start, 3);
     count = 1;
     ptr = start;
-    while(ptr->next != NULL){
+    while (ptr->next != NULL)
+    {
         count++;
         ptr = ptr->next;
     }
-    TEST_ASSERT_EQUAL(2,count);
-
+    TEST_ASSERT_EQUAL(2, count);
 }
 
-void check_delete_rec_file(void){
+void check_delete_rec_file(void)
+{
     id = 1;
-    TEST_ASSERT_EQUAL(SUCCESS, delete_patientrecords_file(&r,&rt,id));
+    TEST_ASSERT_EQUAL(SUCCESS, delete_patientrecords_file(&r, &rt, id));
 }
 
-void check_delete_idx_file(void){
+void check_delete_idx_file(void)
+{
     id = 1;
-    TEST_ASSERT_EQUAL(SUCCESS, delete_patient_index_file(&r,&rt,id));
+    TEST_ASSERT_EQUAL(SUCCESS, delete_patient_index_file(&r, &rt, id));
 }
 
-
-
-void check_uniq_id(void){
+void check_uniq_id(void)
+{
     id = 10;
     strcpy(firstname, "testuser2");
     strcpy(lastname, "lastuser2");
-    strcpy(aadhar_num,"123456789");
+    strcpy(aadhar_num, "123456789");
     height = 167.0;
     weight = 76.1;
     age = 34;
-    strcpy(date,"12-01-21");
+    strcpy(date, "12-01-21");
     insurance = 0;
     shot = 1;
-    insert_patientrecords(start,id,firstname,lastname,aadhar_num,height,weight,age,date,insurance,shot);
-    fseek(f,0,SEEK_END);
-    fwrite(&id,4,1,f);
+    (start, id, firstname, lastname, aadhar_num, height, weight, age, date, insurance, shot);
+    fseek(f, 0, SEEK_END);
+    fwrite(&id, 4, 1, f);
 
     id = 2;
     rewind(f);
-    TEST_ASSERT_EQUAL(SUCCESS,validate_patientid(&f,4));
-    
+    TEST_ASSERT_EQUAL(SUCCESS, validate_patientid(&f, 4));
 }
 
-
- //L_04
-void check_data_loaded_write(void){
+//L_04
+void check_data_loaded_write(void)
+{
     delete_allpatientrecords(start);
     start = loadData(start, &r);
-    TEST_ASSERT_NOT_EQUAL(NULL,start);
-
+    TEST_ASSERT_NOT_EQUAL(NULL, start);
 }
-
-
-
-    
-
-
-
-
 
 int main(void)
 {
@@ -235,13 +228,9 @@ int main(void)
     RUN_TEST(check_delete_idx_file);
     RUN_TEST(check_uniq_id);
     RUN_TEST(check_data_loaded_write);
-    
 
-
-  /* Close the Unity Test Framework */
+    /* Close the Unity Test Framework */
     return UNITY_END();
 }
 
-
-
-//gcc -I inc -I unity unity/unity.c src/create_ll.c src/display_ll.c src/insert_end.c src/delete_box.c src/avg_volume.c src/count_by_color.c src/find_by_id.c src/min_max_v_diff.c src/find_max_height.c src/update_weight.c test/test.c -o testrun.exe   
+//gcc -I inc -I unity unity/unity.c src/create_ll.c src/display_ll.c src/insert_end.c src/delete_box.c src/avg_volume.c src/count_by_color.c src/find_by_id.c src/min_max_v_diff.c src/find_max_height.c src/update_weight.c test/test.c -o testrun.exe

902a5eadc334d2a1bc63be883e4af7a770afa388
Shetty Ganeshprasad <shettyganeshprasad1998@gmail.com>
2021-08-23 10:30:36 +0530

    .

--- 3_Implementation/src/patient_indexFile.c
+++
@@ -1,9 +0,0 @@
-#include "vaccination.h"
-// This file will store nothing more than patient ids to avoid redundant entry.
-error_t patient_indexFile(char *filename, char *mode, FILE **fptr){
-    *fptr = fopen(filename,mode);
-    if( *fptr == NULL){
-        *fptr = fopen(filename,"wb+");
-    }
-    return SUCCESS; 
-}
--- 3_Implementation/src/patient_openFile.c
+++
@@ -1,9 +0,0 @@
-#include "vaccination.h"
-
-error_t patient_openFile(char *filename, char *mode, FILE **fptr){
-    *fptr = fopen(filename,mode);
-    if(*fptr==NULL){
-        *fptr = fopen(filename,"wb+");
-    }    
-    return SUCCESS;
-}

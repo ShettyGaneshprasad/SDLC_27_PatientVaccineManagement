8b36af6bcb3c2e9b5b16ae7a7c2c2b976c5f505d
Amit Kumar Sahni <sahniamitkumar2017@gmail.com>
2021-08-23 08:44:33 +0530

    Add files via upload

---
+++ 3_Implementation/src/patient_readFile.c
@@ -0,0 +1,24 @@
+#include "vaccination.h"
+
+error_t patient_readFile(FILE **fptr){
+    printf("********************READING FROM FILE*****************\n");
+    rewind(*fptr);
+    patient res;
+    long int size = sizeof(res);
+    
+    while(fread(&res, size,1, *fptr)==1){
+        printf("Patient id: %d\n",res.uniq_id);
+        printf("First name: %s\n",res.firstname);
+        printf("last Name: %s\n", res.lastname);
+        printf("Aadhar Number: %s\n", res.aadhar_num);
+        printf("age: %d\n", res.age);
+        printf("Height: %f\n", res.height);
+        printf("Weight: %f\n", res.weight);
+        printf("Insurance Status: %s\n", (res.insurance == 1) ? "Insured" : "Not Insured");
+        printf("Vaccine code: %d\n", res.shot);
+        printf("Date of vaccination is: %s\n", res.date_of_vaccine);
+        printf("*********************\n");        
+    }
+    printf("*****************************END************************\n");
+    return SUCCESS;
+}

5669d9063b4a84c9d04f61e3bdd22e316ddb6c67
srinidhivnayak <49099458+srinidhivnayak@users.noreply.github.com>
2021-08-23 19:52:59 +0530

    Create validate_patientid.c

---
+++ 3_Implementation/src/validate_patientid.c
@@ -0,0 +1,29 @@
+/**
+ * @file validate_patientid.c
+ * @author Srinidhi V Nayak
+ * @version 0.1
+ * @date 2021-08-23
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "vaccination.h"
+
+/**
+ * @brief function to validate patientid
+ *
+ *
+ */
+
+error_t validate_patientid(FILE **fptr, int id)
+{
+    int existing_id;
+    while(fread(&existing_id, 4,1,*fptr)==1)
+    {
+        if(existing_id == id)
+        {
+            return ID_EXISTS;
+        }  
+    }
+    return SUCCESS;
+}

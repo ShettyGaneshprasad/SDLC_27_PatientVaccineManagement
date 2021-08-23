434d6b1d3eb69dbe074e2a0cab2e9d823c791ab8
Shetty Ganeshprasad <shettyganeshprasad1998@gmail.com>
2021-08-23 16:00:47 +0530

    added function to delete patient

---
+++ 3_Implementation/src/delete_all.c
@@ -0,0 +1,47 @@
+/**
+ * @file delete_all.c
+ * @author Shetty Ganeshprasad (shettyganeshprasad1998@gmail.com)
+ * @brief function to delete patient from the record
+ * @version 0.1
+ * @date 2021-08-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "vaccination.h"
+
+/**
+ * @brief delete all patient from record
+ * 
+ * @param start 
+ */
+
+void delete_all(patient *start)
+{
+
+    patient *current;
+    current = start;
+    patient *ptr;
+    while (current != NULL)
+    {
+        ptr = current->next;
+        free(current);
+        current = ptr;
+    }
+    start = NULL;
+}
+
+/**
+ * @brief  delete starting patient record
+ * 
+ * @param start 
+ * @return patient* 
+ */
+patient *delete_beg(patient *start)
+{
+    patient *ptr;
+    ptr = start;
+    start = start->next;
+    free(ptr);
+    return start;
+}
